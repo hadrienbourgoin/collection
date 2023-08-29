a4a771c427363b1286b86d97847b2b8f663d9dd5
andy mputu <andymputu18@gmail.com>
2023-08-29 18:12:22 +0200

    tmp

--- app/controllers/compilations_controller.rb
+++ app/controllers/compilations_controller.rb
@@ -8,6 +8,14 @@ class CompilationsController < ApplicationController
     end
   end
 
+  def destroy
+    @compilation = Compilation.find(params[:id])
+    @compilation.destroy
+    redirect_to compilations_path, status: :see_other
+  end
+
+  private
+
   def compilation_params
     params.require(:compilation).permit(:name, :description, :public)
   end
