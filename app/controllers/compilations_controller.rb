9b82de8b975b4ae9fda653c259f27d9ae7946aed
Sébastien Mercier-Bellevue <sebastien.mercierbellevue@gmail.com>
2023-08-29 17:12:11 +0200

    new create edit update

--- app/controllers/compilations_controller.rb
+++ app/controllers/compilations_controller.rb
@@ -1,4 +1,6 @@
 class CompilationsController < ApplicationController
+  before_action :set_compilation, only: [:show, :edit, :update]
+
   def index
     @compilations = Compilation.all
     if params[:browse] == "true"
@@ -6,6 +8,43 @@ class CompilationsController < ApplicationController
     else
       @browse = false
     end
+  end
+
+  def show
+    @compilation.user = current_user
+  end
+ 
+  def new
+    @compilation = Compilation.new
+  end
+  
+  def create
+    @compilation = Compilation.new(compilation_params)
+    @compilation.user = current_user
+    if @compilation.save
+      redirect_to compilation_path(@compilation)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @compilation.user = current_user
+  end
+
+  def update
+    if @compilation.update(compilation_params)
+      redirect_to compilation_path(@compilation)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def set_compilation
+    @compilation = Compilation.find(params[:id])
   end
 
   def compilation_params
